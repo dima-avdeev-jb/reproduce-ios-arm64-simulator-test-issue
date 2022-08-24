52ac9b50f74b5a921ba9892cd7bcddcd89455c80
dima.avdeev <dima.avdeev@jetbrains.com>
2022-08-24 13:33:49 +0400

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My_Application"
+include(":shared")
---
+++ shared/src/commonTest/kotlin/com/example/myapplication/FailedTest.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FailedTest {
+
+    @Test
+    fun failedTest() {
+        assertEquals(0, 1)
+    }
+}
